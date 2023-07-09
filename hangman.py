45a50daa38cebf7d8821a758c35130ef878b7a40
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2023-07-10 01:28:23 +0300

    ASCII hangman integrated, few conditions adjusted

--- hangman.py
+++ hangman.py
@@ -1,4 +1,62 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["hello", "world", "python"]
 chosen_word = random.choice(word_list)
 word_lenght = len(chosen_word)
@@ -11,18 +69,33 @@ display = []
 for blank in range(word_lenght):
     display += "_"
 
+lives = 6
+
 end_of_the_game = False
 while not end_of_the_game:
     guess = input("Guess a letter: ").lower()
-
+    check = True
     # Check guessed letter
     for position in range(word_lenght):
         letter = chosen_word[position]
         
         if letter == guess:
+            check = False
             display[position] = letter
+    # Wrong letter takes a life
+    if check == True:
+        lives -= 1
+    # Game over condition when lives runs out
+    if lives == 0:
+        end_of_the_game = True
+        print("You died.")
+    # Concatenate all items in list and convert to string
+    print(f"{' '.join(display)}")
     
-    print(display)
+    # Check if user has got all letters
     if "_" not in display:
         end_of_the_game = True
         print("You win!")
+
+    # ASCII art corresponding to current lives
+    print(stages[lives])
